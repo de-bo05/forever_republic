066cf1e4349fe3ff5bf31b471ead81bc2f0faca9
Ajibewa Iyiola <deboroajibewa@gmail.com>
2023-08-23 03:31:22 +0100

    y

---
+++ lib/db_reference/Category.dart
@@ -0,0 +1,11 @@
+
+// ignore_for_file: constant_identifier_names
+
+enum Category{
+  table,
+  database,
+  id,
+  created_at,
+  name,
+  parent_id
+}
---
+++ lib/db_reference/Product.dart
@@ -0,0 +1,13 @@
+
+// ignore_for_file: constant_identifier_names
+
+enum Product{
+  table,
+  database,
+  id,
+  created_at,
+  name,
+  description,
+  price,
+  discount,
+}
